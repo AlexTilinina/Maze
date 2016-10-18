5d3dc2ded337e1bcccb21ac0c4cee18e3b42789b
vladislavtoporov <vladislavtoporov@gmail.com>
2016-10-18 17:25:58 +0300

    added class GameMap.java

---
+++ GameMap.java
@@ -0,0 +1,23 @@
+// package Maze.game;
+
+abstract class GameMap {
+    
+    protected int[][] map;
+   
+    public boolean hasTile(int x, int y) {
+       return (x >= 0 && y >= 0 && y <  getHeight() && x <  getWidth()); 
+    }
+    
+    public int getTileId(int x, int y) {
+        return hasTile(x,y) ? map[y][x] : 0;
+    }
+    
+    public int getHeight() {
+        return map.length;
+    }
+    
+   
+    public int getWidth() {
+        return map[0].length;
+    }
+}
