99c97a3cbdf44c2cfae440687d041df39a5b3fce
vladislavtoporov <vladislavtoporov@gmail.com>
2016-10-18 17:26:39 +0300

    added class Map2.java

---
+++ Map2.java
@@ -0,0 +1,14 @@
+package Maze.game;
+
+public class Map2 extends GemaMap {
+     public Map2() {
+        map = new int[][]{ 
+            {1,1,1,1,1,1,1},
+            {1,2,2,2,2,2,1},
+            {1,2,1,1,1,2,1},
+            {1,2,1,4,1,2,1},
+            {1,2,2,2,2,2,1},
+            {1,1,1,1,1,1,1}
+        };
+    }   
+}
